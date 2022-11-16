606a2a93f5bcb9292244dc34aa56184942ba6891
seongzzang <87599027+seongzzang@users.noreply.github.com>
2022-11-16 23:42:48 +0900

    update_feedTableViewCell

--- Outsidestargram/Scenes/FeedViewController.swift
+++ Outsidestargram/Scenes/FeedViewController.swift
@@ -9,14 +9,41 @@ import UIKit
 import SnapKit
 
 class FeedViewController: UIViewController {
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero)
+        tableView.backgroundColor = .systemBackground
+        tableView.separatorStyle = .none
+        tableView.dataSource = self
+        
+        tableView.register(FeedTableViewCell.self, forCellReuseIdentifier: "FeedTableViewCell")
+        
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupNavigationBar()
+        setupTableView()
     }
 
 }
+
+extension FeedViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "FeedTableViewCell", for: indexPath) as? FeedTableViewCell else { return UITableViewCell() }
+        cell.selectionStyle = .none
+        cell.setup()
+        
+        return cell
+    }
+}
+
 
 private extension FeedViewController {
 
@@ -28,7 +55,11 @@ private extension FeedViewController {
                                            target: self,
                                            action: nil)
         navigationItem.rightBarButtonItem = uploadButton
-        
+    }
+    
+    func setupTableView() {
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { $0.edges.equalToSuperview() }
     }
 }
 
