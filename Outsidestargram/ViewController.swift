cde79fde45d5ea62a8438b91b698d72236a3c4dc
seongzzang <87599027+seongzzang@users.noreply.github.com>
2022-11-16 22:14:24 +0900

    Initial Commit

---
+++ Outsidestargram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Outsidestargram
+//
+//  Created by 양성혜 on 2022/11/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
