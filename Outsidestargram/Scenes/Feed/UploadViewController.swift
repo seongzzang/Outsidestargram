26edc4ee74b328de97754bd865f509eb99c3b4c4
seongzzang <87599027+seongzzang@users.noreply.github.com>
2022-11-17 22:17:13 +0900

    update_uploadVIewController

---
+++ Outsidestargram/Scenes/Feed/UploadViewController.swift
@@ -0,0 +1,17 @@
+//
+//  UploadViewController.swift
+//  Outsidestargram
+//
+//  Created by 양성혜 on 2022/11/17.
+//
+
+import UIKit
+import SnapKit
+
+final class UploadViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
