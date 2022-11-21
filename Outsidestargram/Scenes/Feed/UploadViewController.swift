5fdeb4b4802dd619f4b5174b68579542c57ed49c
seongzzang <87599027+seongzzang@users.noreply.github.com>
2022-11-21 22:56:30 +0900

    uploadView_setupLayout

--- Outsidestargram/Scenes/Feed/UploadViewController.swift
+++ Outsidestargram/Scenes/Feed/UploadViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 import SnapKit
 
 final class UploadViewController: UIViewController {
+    
+    private let imageView = UIImageView()
+    
+    private lazy var textView : UITextView = {
+       let textView = UITextView()
+        textView.font = .systemFont(ofSize: 15.0)
+        
+        return textView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemBackground
         setupNavigationItem()
+        setupLayout()
     }
 }
 
@@ -38,4 +49,27 @@ private extension UploadViewController {
     @objc func didTapRgihtButton() {
         dismiss(animated: true)
     }
+    
+    func setupLayout() {
+        [imageView, textView].forEach {view.addSubview($0)}
+        
+        let imageViewInset: CGFloat = 16.0
+        
+        imageView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).inset(imageViewInset)
+            $0.leading.equalToSuperview().inset(imageViewInset)
+            $0.width.equalTo(100.0)
+            $0.height.equalTo(imageView.snp.width)
+        }
+        
+        textView.snp.makeConstraints {
+            $0.leading.equalTo(imageView.snp.trailing).offset(imageViewInset)
+            $0.trailing.equalToSuperview().inset(imageViewInset)
+            $0.top.equalTo(imageView.snp.top)
+            $0.bottom.equalTo(imageView.snp.bottom)
+        }
+    }
+
 }
+
+
