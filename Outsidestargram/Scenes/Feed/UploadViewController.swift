79ef109a95635859e24843d5a3e3c7f7b5e25ec8
seongzzang <87599027+seongzzang@users.noreply.github.com>
2022-11-21 23:05:59 +0900

    setting_uploadView_image&textView

--- Outsidestargram/Scenes/Feed/UploadViewController.swift
+++ Outsidestargram/Scenes/Feed/UploadViewController.swift
@@ -10,14 +10,29 @@ import SnapKit
 
 final class UploadViewController: UIViewController {
     
+    private let uploadImage: UIImage
+    
     private let imageView = UIImageView()
     
     private lazy var textView : UITextView = {
        let textView = UITextView()
+        textView.text = "문구 입력..."
+        textView.textColor = .secondaryLabel
         textView.font = .systemFont(ofSize: 15.0)
+        textView.delegate = self
         
         return textView
     }()
+    
+    init(uploadImage: UIImage) {
+        self.uploadImage = uploadImage
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +40,17 @@ final class UploadViewController: UIViewController {
         view.backgroundColor = .systemBackground
         setupNavigationItem()
         setupLayout()
+    
+        imageView.image = uploadImage
+    }
+}
+
+extension UploadViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        guard textView.textColor == .secondaryLabel else {return}
+        
+        textView.text = nil
+        textView.textColor = .label
     }
 }
 
