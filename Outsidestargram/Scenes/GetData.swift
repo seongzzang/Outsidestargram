becf4bb33f3c314df164597da6f2c3cce8b38a1e
seongzzang <87599027+seongzzang@users.noreply.github.com>
2022-12-08 23:28:40 +0900

    update_class

---
+++ Outsidestargram/Scenes/GetData.swift
@@ -0,0 +1,16 @@
+//
+//  GetData.swift
+//  Outsidestargram
+//
+//  Created by 양성혜 on 2022/12/08.
+//
+
+import UIKit
+
+class GetData {
+    //get  data
+    
+    func getData(){
+        //example firebase
+    }
+}
