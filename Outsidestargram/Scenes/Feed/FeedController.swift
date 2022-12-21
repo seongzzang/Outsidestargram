5409e7d2ee49ebb3e00a172c3628282f1ead065c
seongzzang <87599027+seongzzang@users.noreply.github.com>
2022-12-21 23:30:44 +0900

    update_about_scrollCell

--- Outsidestargram/Scenes/Feed/FeedController.swift
+++ Outsidestargram/Scenes/Feed/FeedController.swift
@@ -12,13 +12,49 @@ class FeedController: UIViewController {
 
     private lazy var scrollView = UIScrollView()
     
-    let feedScrollView = FeedScrollViewCell(frame: .zero)
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero)
+        tableView.backgroundColor = .systemBackground
+        tableView.separatorStyle = .none
+        tableView.dataSource = self
+        
+        tableView.register(FeedScrollViewCell.self, forCellReuseIdentifier: "FeedScrollViewCell")
+        
+        return tableView
+    }()
+    
     let feedView = FeedViewController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTableView()
     }
     
+    
+    
+}
+
+extension FeedController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "FeedScrollViewCell", for: indexPath) as? FeedScrollViewCell else { return UITableViewCell() }
+        cell.selectionStyle = .none
+        cell.setupLayout()
+        
+        return cell
+    }
+    
+}
+
+extension FeedController: UITableViewDelegate {
+    
+}
+
+private extension FeedController {
     
     func setupTableView() {
         view.addSubview(scrollView)
@@ -29,8 +65,8 @@ class FeedController: UIViewController {
             $0.height.equalTo(100)
         }
         
-        scrollView.addSubview(feedScrollView)
-        feedScrollView.snp.makeConstraints {
+        scrollView.addSubview(tableView)
+        tableView.snp.makeConstraints {
             $0.top.equalTo(scrollView.snp.top).offset(5.0)
             $0.leading.equalTo(scrollView).offset(5.0)
             $0.trailing.equalTo(scrollView).offset(5.0)
@@ -45,5 +81,5 @@ class FeedController: UIViewController {
 //            $0.trailing.equalToSuperview()
 //        }
     }
-    
 }
+
