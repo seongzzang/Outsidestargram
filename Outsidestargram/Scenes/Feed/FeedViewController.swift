9eb4af1e4f37b040b7a180d519e5f4d681475d48
seongzzang <87599027+seongzzang@users.noreply.github.com>
2022-12-23 23:56:38 +0900

    change_the cell

--- Outsidestargram/Scenes/Feed/FeedViewController.swift
+++ Outsidestargram/Scenes/Feed/FeedViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import SnapKit
 
-class FeedViewController: UIView {
+class FeedViewController: UIViewController {
  
     private lazy var tableView: UITableView = {
         let tableView = UITableView(frame: .zero)
